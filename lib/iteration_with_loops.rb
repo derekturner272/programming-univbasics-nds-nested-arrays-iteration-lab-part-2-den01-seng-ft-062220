24b184af1d545b797df36c1a45a3cc878a1d1391
Derek Turner <turner.derek.b@gmail.com>
2020-06-05 04:59:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
     dayly_low = 1000
     while temp_index < src[day_index].count do
       if src[day_index][temp_index] < dayly_low
-        
+        dayly_low = src[day_index][temp_index]
       end
       temp_index += 1
     end
